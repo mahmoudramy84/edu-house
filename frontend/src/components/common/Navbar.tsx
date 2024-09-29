d782a17287fcd23849029f73917024cae9f1628c
Safeya-Yasien <ssafeya5@gmail.com>
2024-09-29 12:43:18 +0300

    add main pages

--- frontend/src/components/common/Navbar.tsx
+++ frontend/src/components/common/Navbar.tsx
@@ -1,10 +1,47 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { FaShoppingBag } from "react-icons/fa";
+import { FaMagnifyingGlass } from "react-icons/fa6";
 
 const Navbar = () => {
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <div className="flex flex-wrap items-center justify-between mx-auto p-4 px-12">
+        <Link
+          href="/"
+          className="flex items-center space-x-3 rtl:space-x-reverse"
+        >
+          <Image src="" className="h-8" alt="Logo" />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            Logo
+          </span>
+        </Link>
+        <button
+          data-collapse-toggle="navbar-default"
+          type="button"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="navbar-default"
+          aria-expanded="false"
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-5 h-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        </button>
+        {/* links */}
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
@@ -18,21 +55,70 @@ const Navbar = () => {
             </li>
             <li>
               <Link
-                href="/auth/login"
+                href="/courses"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
-                Login
+                Courses
               </Link>
             </li>
             <li>
               <Link
-                href="/auth/signup"
+                href="/events"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
-                Singup
+                Events
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/blog"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                Blog
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/contact"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                Contact
               </Link>
             </li>
           </ul>
+        </div>
+        <div className="flex items-center gap-4">
+          {/* search */}
+          <div
+            className="ring-2 ring-gray-300 rounded-full px-2 text-xs
+        flex items-center"
+          >
+            <FaMagnifyingGlass className="text-gray-400" cursor="pointer" />
+            <input
+              type="text"
+              placeholder="Search..."
+              className="outline-none w-[150px] bg-transparent p-2"
+            />
+          </div>
+          {/* shopping cart */}
+          <div className=" w-8 h-8 rounded-full ring-2 ring-gray-300 flex items-center justify-center cursor-pointer relative ">
+            <FaShoppingBag />
+            <span
+              className="bg-red-500 w-5 h-5 rounded-full flex items-center justify-center text-white font-semibold text-sm
+            absolute -top-2 -right-2"
+            >
+              2
+            </span>
+          </div>
+          {/* login, signup */}
+          <div className="flex items-center gap-4">
+            <Link
+              href="/auth/login"
+              className="bg-[#140342] dark:bg-[#40009A] text-white rounded-lg   px-4 py-2 text-sm"
+            >
+              Login
+            </Link>
+          </div>
         </div>
       </div>
     </nav>
