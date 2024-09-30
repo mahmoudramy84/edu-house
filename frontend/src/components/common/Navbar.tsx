95e72368e88efab712a475f04c56482ec5d22db3
Safeya-Yasien <ssafeya5@gmail.com>
2024-09-30 09:57:48 +0300

    Add responsive ToggleMenuButton to Navbar and improve layout styling

--- frontend/src/components/common/Navbar.tsx
+++ frontend/src/components/common/Navbar.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { FaShoppingBag } from "react-icons/fa";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import ThemeSwitcher from "./ThemeSwitcher";
+import ToggleMenuButton from "../ToggleMenuButton";
 
 const Navbar = () => {
   return (
@@ -11,39 +12,16 @@ const Navbar = () => {
       <div className="flex flex-wrap items-center justify-between mx-auto p-4 px-12">
         <Link
           href="/"
-          className="flex items-center space-x-3 rtl:space-x-reverse"
+          className="flex items-center space-x-3 rtl:space-x-reverse mx-auto mb-5 sm:mb-0 sm:m-0 "
         >
           <Image src="" className="h-8" alt="Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             Logo
           </span>
         </Link>
-        <button
-          data-collapse-toggle="navbar-default"
-          type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-default"
-          aria-expanded="false"
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M1 1h15M1 7h15M1 13h15"
-            />
-          </svg>
-        </button>
+
         {/* links */}
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+        <div className="hidden w-full lg:block md:w-auto">
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <Link
@@ -88,7 +66,7 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
-        <div className="flex items-center gap-4 flex-wrap">
+        <div className="flex items-center gap-4 flex-wrap w-full sm:w-auto justify-between sm:justify-end">
           {/* search */}
           <div
             className="ring-2 ring-gray-300 rounded-full px-2 text-xs
@@ -115,12 +93,13 @@ const Navbar = () => {
           <div className="flex items-center gap-4">
             <Link
               href="/auth/login"
-              className="bg-[#140342] dark:bg-[#40009A] text-white rounded-lg   px-4 py-2 text-sm"
+              className="bg-[#5E199A] dark:bg-purple-800 text-white rounded-lg   px-4 py-2 text-sm"
             >
               Login
             </Link>
           </div>
           <ThemeSwitcher />
+          <ToggleMenuButton />
         </div>
       </div>
     </nav>
