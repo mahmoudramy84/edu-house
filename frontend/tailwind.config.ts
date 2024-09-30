4e63f57fb08a9aebf4df67f095556d453406b23c
Safeya-Yasien <ssafeya5@gmail.com>
2024-09-30 15:13:03 +0300

    Add framer-motion and swiper dependencies; update tsconfig and tailwind config
    - Added `framer-motion` and `swiper` to dependencies in `package.json` and `package-lock.json`.
    - Updated `tsconfig.json` formatting for better readability.
    - Modified `tailwind.config.ts` to include a custom purple color.

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -9,8 +9,7 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        customPurple:"#5E199A"
       },
     },
   },
