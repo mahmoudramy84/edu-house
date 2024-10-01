4194382095c51378143baf42424b750b77625bb8
Safeya-Yasien <ssafeya5@gmail.com>
2024-10-01 12:09:35 +0300

    Reorder components in `page.tsx` for better layout flow

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -20,8 +20,8 @@ export default function Home() {
       <WhyChooseUs />
       <BestInstructors />
       <StudentTestimonials />
-      <UpcomingEvents />
       <BecomeAStudentOrInstructor />
+      <UpcomingEvents />
     </>
   );
 }
