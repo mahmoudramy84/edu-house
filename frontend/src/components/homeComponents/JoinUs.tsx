a6dca1aaaf2a9480dc7c3b57a073bc6d040ec222
Safeya-Yasien <ssafeya5@gmail.com>
2024-10-01 11:25:40 +0300

    Enhance `ButtonLink` component in `JoinUs` section with additional props for margin and background control

--- frontend/src/components/homeComponents/JoinUs.tsx
+++ frontend/src/components/homeComponents/JoinUs.tsx
@@ -3,14 +3,14 @@ import { ButtonLink, SectionHeader } from "../common";
 
 const JoinUs = () => {
   return (
-    <section className="py-16 bg-lightGray dark:bg-gray-900">
+    <section className="py-16 bg-lightGray dark:bg-gray-900 ">
       <div className="container mx-auto text-center">
         <SectionHeader
           title="Find the right learning path for you"
           description="Match your goals to our programs, explore your options, and map out your path to success."
         />
 
-        <ButtonLink href="#"> Get Started Now</ButtonLink>
+        <ButtonLink href="#" addMargin={true} withoutBg={true}> Get Started Now</ButtonLink>
       </div>
     </section>
   );
