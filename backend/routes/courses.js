ecc95b2787198fcb5243aaf136c452e62b64d847
mahmoudramy84 <mahmoudramy84@gmail.com>
2024-10-10 15:45:39 +0300

    Implement Protect Routes Using JWT Middleware& implement (RBAC)

--- backend/routes/courses.js
+++ backend/routes/courses.js
@@ -2,20 +2,21 @@
 const express = require('express');
 const router = express.Router();
 const { addCourse, getCourses, getCourseById, updateCourse, deleteCourse } = require('../controllers/courseController');
+const { verifyToken, requireRole } = require('../middleware/authMiddleware');
 
 // Create a new course
-router.post('/', addCourse);
+router.post('/', verifyToken, requireRole('instructor'), addCourse);
 
 // Get all courses
-router.get('/', getCourses);
+router.get('/', verifyToken, getCourses);
 
 // Get a course by ID
-router.get('/:id', getCourseById);
+router.get('/:id', verifyToken, getCourseById);
 
 // Update a course
-router.put('/:id', updateCourse);
+router.put('/:id', verifyToken, requireRole('instructor'), updateCourse);
 
 // Delete a course
-router.delete('/:id', deleteCourse);
+router.delete('/:id', verifyToken, requireRole('admin'), deleteCourse);
 
 module.exports = router;
